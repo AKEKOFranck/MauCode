a30eb9c8b45ab538ba828052d7e0c6fd80a4c2a8
AKEKOFranck <142504737+MauriceAkeko@users.noreply.github.com>
2025-08-20 10:38:13 +0000

    fifth test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,8 @@ import '../styles/Footer.css';
 export default function Footer() {
     return(
         <footer>
-            
+            <p>pour plus d'infos</p>
+            <p>fa0055970@gmail.com</p>
         </footer>
     )
 }
